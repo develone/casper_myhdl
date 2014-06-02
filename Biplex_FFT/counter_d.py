32f62d38193dab1e9d8cca6c8f0613467df5e74d
Edward Vidal Jr <develone@sbcglobal.net>
2014-06-02 14:58:20 -0600

    counter_out was removed such that counter_logic is returning a single value.
    // File: counter_d.v
    // Generated by MyHDL 0.9dev
    // Date: Mon Jun  2 14:32:31 2014
    
    `timescale 1ns/10ps
    
    module counter_d (
        cnt,
        clk,
        ena,
        rst,
        updown,
        step,
        MIN_COUNT,
        MAX_COUNT
    );
    
    output [15:0] cnt;
    reg [15:0] cnt;
    input clk;
    input ena;
    input rst;
    input [15:0] updown;
    input step;
    input [8:0] MIN_COUNT;
    input [15:0] MAX_COUNT;
    
    always @(posedge clk) begin: COUNTER_D_COUNTER_LOGIC
        if ((rst == 1)) begin
            cnt <= 0;
        end
        else if ((ena == 1)) begin
            if ((updown == 1)) begin
                if (($signed({1'b0, cnt}) == ($signed({1'b0, MIN_COUNT}) - 1))) begin
                    cnt <= 0;
                end
                else begin
                    cnt <= (cnt + step);
                end
            end
            else begin
                if ((cnt == (MIN_COUNT + 1))) begin
                    cnt <= MAX_COUNT;
                end
                else begin
                    cnt <= (cnt - step);
                end
            end
        end
        cnt <= cnt;
    end
    
    endmodule

--- Biplex_FFT/counter_d.py
+++ Biplex_FFT/counter_d.py
@@ -6,10 +6,9 @@ def ClkDriver(clk):
                 clk.next = not clk
         return driveClk
 
-def counter_d(count_out, clk, ena, rst, updown = 1, step = 1, MIN_COUNT = 0, MAX_COUNT = 65536 ):
+def counter_d(cnt, clk, ena, rst, updown = 1, step = 1, MIN_COUNT = 0, MAX_COUNT = 65536 ):
     
     
-    #limit = Signal(intbv()[(MAX_COUNT):MIN_COUNT])
     limit = int()
     
     if updown == 1:
@@ -17,9 +16,6 @@ def counter_d(count_out, clk, ena, rst, updown = 1, step = 1, MIN_COUNT = 0, MAX
     else:
         limit = MIN_COUNT
         
-    direction = Signal(bool())
-    #updown_1 = updown_d(limit, direction, updown, MIN_COUNT, MAX_COUNT)
-    cnt = Signal(intbv(0)[16:])
     
     @always(clk.posedge)
     def counter_logic():
@@ -39,19 +35,17 @@ def counter_d(count_out, clk, ena, rst, updown = 1, step = 1, MIN_COUNT = 0, MAX
                     cnt.next = MAX_COUNT
                 else:
                     cnt.next = cnt - step
-        count_out = cnt   
+        cnt.next = cnt   
     return counter_logic
-def convert():
-        MAX_COUNT = 65536
-        MIN_COUNT = 0
-        count_out = Signal(intbv()[16:]) 
-        cnt = Signal(intbv(0)[16:])
-        updown = Signal(bool())
-        ena = Signal(bool())
-        rst = Signal(bool())
-        clk = Signal(bool())
-        step = Signal(intbv()[9:0])
-        toVerilog(counter_d, count_out, clk, ena, rst, MAX_COUNT, updown, step, MIN_COUNT)
+MAX_COUNT = Signal(intbv(65536)[16:])
+MIN_COUNT = Signal(intbv(0)[16:])
+cnt = Signal(intbv(0)[16:])
+updown = Signal(bool())
+ena = Signal(bool())
+rst = Signal(bool())
+clk = Signal(bool())
+step = Signal(intbv(1)[9:])
+toVerilog(counter_d, cnt, clk, ena, rst, MAX_COUNT, updown, step, MIN_COUNT)
 
 def test_bench():
         clk = Signal(0)
@@ -66,6 +60,5 @@ def test_bench():
         #tb = traceSignals(test_bench)
         sim = Simulation(clkdriver_inst, counter_inst)
         sim.run(50)
-convert()
-test_bench()
+#test_bench()
 
