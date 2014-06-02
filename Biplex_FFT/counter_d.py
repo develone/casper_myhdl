55522589d6c72bf228329d9e66b43428eeb91329
Edward Vidal Jr <develone@sbcglobal.net>
2014-06-02 08:27:00 -0600

    first try at generating verilog and adding a simulation.

--- Biplex_FFT/counter_d.py
+++ Biplex_FFT/counter_d.py
@@ -1,4 +1,12 @@
-def counter_d(count_out, clk, ena, rst, updown, step, MAX_COUNT, MIN_COUNT = 0 ):
+from myhdl import *
+def ClkDriver(clk):
+        halfPeriod = delay(10)
+        @always(halfPeriod)
+        def driveClk():
+                clk.next = not clk
+        return driveClk
+
+def counter_d(count_out, clk, ena, rst, updown = 1, step = 1, MIN_COUNT = 0, MAX_COUNT = 65536 ):
     
     
     #limit = Signal(intbv()[(MAX_COUNT):MIN_COUNT])
@@ -11,7 +19,7 @@ def counter_d(count_out, clk, ena, rst, updown, step, MAX_COUNT, MIN_COUNT = 0 )
         
     direction = Signal(bool())
     #updown_1 = updown_d(limit, direction, updown, MIN_COUNT, MAX_COUNT)
-    cnt = Signal(intbv(0)[int((len(count_out))):])
+    cnt = Signal(intbv(0)[16:])
     
     @always(clk.posedge)
     def counter_logic():
@@ -31,5 +39,33 @@ def counter_d(count_out, clk, ena, rst, updown, step, MAX_COUNT, MIN_COUNT = 0 )
                     cnt.next = MAX_COUNT
                 else:
                     cnt.next = cnt - step
-        count_out.next = cnt   
+        count_out = cnt   
     return counter_logic
+def convert():
+        MAX_COUNT = 65536
+        MIN_COUNT = 0
+        count_out = Signal(intbv()[16:]) 
+        cnt = Signal(intbv(0)[16:])
+        updown = Signal(bool())
+        ena = Signal(bool())
+        rst = Signal(bool())
+        clk = Signal(bool())
+        step = Signal(intbv()[9:0])
+        toVerilog(counter_d, count_out, clk, ena, rst, MAX_COUNT, updown, step, MIN_COUNT)
+
+def test_bench():
+        clk = Signal(0)
+        ena = 1
+        rst = 1
+        updown = 1
+        cnt  = 0
+        count_out = 0
+        step = 1
+        counter_inst = counter_d(count_out, clk, ena, rst, MAX_COUNT = 65536, updown = 1, step = 1, MIN_COUNT = 0 )
+        clkdriver_inst = ClkDriver(clk)
+        #tb = traceSignals(test_bench)
+        sim = Simulation(clkdriver_inst, counter_inst)
+        sim.run(50)
+convert()
+test_bench()
+
