6cc28848ba5d240359b8f66a0602796bf815bece
Edward Vidal Jr <develone@sbcglobal.net>
2014-06-03 07:56:36 -0600

    removed sp trying to get cmd to work

--- Biplex_FFT/testcounter_d.py
+++ Biplex_FFT/testcounter_d.py
@@ -2,6 +2,6 @@ import os
 from myhdl import Cosimulation
 cmd = "/usr/local/bin/iverilog -o counter_d counter_d.v tb_counter_d.v"
 print cmd
-def counter_d (cnt,clk,ena,rst,updown,step,MIN_COUNT,MAX_COUNT):
+def counter_d(cnt,clk,ena,rst,updown,step,MIN_COUNT,MAX_COUNT):
 	os.system(cmd)
 	return Cosimulation("/usr/local/bin/vvp -m ./myhdl.vpi counter_d")
