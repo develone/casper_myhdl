cbe18024aea15b261d4b3a45f81d19ed209b2806
Edward Vidal <vidal@sim4.swbell.net>
2014-06-03 18:02:23 -0600

    adding the verilog files generated by python counter_d.py

---
+++ Biplex_FFT/tb_counter_d.v
@@ -0,0 +1,38 @@
+module tb_counter_d;
+
+wire [35:0] cnt;
+reg clk;
+reg ena;
+reg rst;
+reg [35:0] updown;
+reg step;
+reg [8:0] MIN_COUNT;
+reg [35:0] MAX_COUNT;
+
+initial begin
+    $from_myhdl(
+        clk,
+        ena,
+        rst,
+        updown,
+        step,
+        MIN_COUNT,
+        MAX_COUNT
+    );
+    $to_myhdl(
+        cnt
+    );
+end
+
+counter_d dut(
+    cnt,
+    clk,
+    ena,
+    rst,
+    updown,
+    step,
+    MIN_COUNT,
+    MAX_COUNT
+);
+
+endmodule
