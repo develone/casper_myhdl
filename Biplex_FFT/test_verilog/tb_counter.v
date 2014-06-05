75b5ec3b5fd6f2f16202b69f0fae802b264a9d43
Edward Vidal Jr <develone@sbcglobal.net>
2014-06-05 13:45:46 -0600

    back use intbv instead of fixbv using vvp and iverilog with GTKWave vvp test_counter VCD info: dumpfile tb_counter.vcd opened for output. At time                    0, count_out  = xxxxx (x) At time                 3500, count_out  = 00000 (0) At time                 4500, count_out  = 00001 (1) At time                 5500, count_out  = 00000 (0) At time                 6500, count_out  = 1ffff (131071) At time                 7500, count_out  = 00000 (0) At time                 8500, count_out  = 00001 (1) At time                 9500, count_out  = 00002 (2) At time                10500, count_out  = 00003 (3) At time                11500, count_out  = 00004 (4) At time                12500, count_out  = 00005 (5) At time                13500, count_out  = 00006 (6) At time                14500, count_out  = 00007 (7) At time                15500, count_out  = 00008 (8) At time                16500, count_out  = 00009 (9) ** VVP Stop(0) ** ** Flushing output streams. ** Current simulation time is 16700 ticks. simulation is working

--- Biplex_FFT/test_verilog/tb_counter.v
+++ Biplex_FFT/test_verilog/tb_counter.v
@@ -1,22 +1,40 @@
 module tb_counter;
 
-wire [16:0] count_out;
-reg clk;
-reg ena;
-reg rst;
-reg updown;
+initial
+ begin
+    $dumpfile("tb_counter.vcd");
+    $dumpvars(0,tb_counter);
+ end
 
-initial begin
-    $from_myhdl(
-        clk,
-        ena,
-        rst,
-        updown
-    );
-    $to_myhdl(
-        count_out
-    );
-end
+  /* Make a updown that pulses once. */
+  reg updown = 0;
+  initial begin
+     # 27 updown = 1;
+     # 11 updown = 0;
+     # 29 updown = 1;
+     # 100 $stop;
+  end
+  /* Make a rst that pulses once. */
+  reg rst = 0;
+  initial begin
+     # 17 rst = 1;
+     # 11 rst = 0;
+     # 29 rst = 1;
+     # 11 rst = 0;
+     # 100 $stop;
+  end
+
+  /* Make a ena that pulses once. */
+  reg ena = 0;
+  initial begin
+     # 17 ena = 1;
+  end
+ /* Make a regular pulsing clock. */
+  reg clk = 0;
+  always #5 clk = !clk;
+
+wire [16:0] count_out;
+
 
 counter dut(
     count_out,
@@ -25,5 +43,8 @@ counter dut(
     rst,
     updown
 );
+ initial
+     $monitor("At time %t, count_out  = %h (%0d)",
+              $time, count_out, count_out);
 
 endmodule
