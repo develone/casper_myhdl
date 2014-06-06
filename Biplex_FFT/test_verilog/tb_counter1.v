5efc880330e45828dd210180d8488edd789592d3
Edward Vidal <vidal@sim4.swbell.net>
2014-06-05 18:26:32 -0600

    fixbv appears to work now with count_out 1 less than cnt by 1 clk

--- Biplex_FFT/test_verilog/tb_counter1.v
+++ Biplex_FFT/test_verilog/tb_counter1.v
@@ -1,22 +1,39 @@
 module tb_counter1;
+initial
+ begin
+    $dumpfile("tb_counter1.vcd");
+    $dumpvars(0,tb_counter1);
+ end
 
+  /* Make a updown that pulses once. */
+  reg updown = 0;
+  initial begin
+     # 27 updown = 1;
+     # 11 updown = 0;
+     # 29 updown = 1;
+     # 100 $stop;
+  end
+  /* Make a rst that pulses once. */
+  reg rst = 0;
+  initial begin
+     # 17 rst = 1;
+     # 11 rst = 0;
+     # 29 rst = 1;
+     # 11 rst = 0;
+  end
+ /* Make a regular pulsing clock. */
+  reg clk = 0;
+  always #5 clk = !clk;
 wire [33:0] count_out;
-reg clk;
 reg ena;
-reg rst;
-reg updown;
 
-initial begin
-    $from_myhdl(
-        clk,
-        ena,
-        rst,
-        updown
-    );
-    $to_myhdl(
-        count_out
-    );
-end
+
+  /* Make a ena that pulses once. */
+  initial begin
+     # 17 ena = 1;
+  end
+ /* Make a regular pulsing clock. */
+  always #5 clk = !clk;
 
 counter1 dut(
     count_out,
@@ -25,5 +42,9 @@ counter1 dut(
     rst,
     updown
 );
+
+initial
+     $monitor("At time %t, count_out  = %h (%0d)",
+              $time, count_out, count_out);
 
 endmodule
